faab27a270f546f2e9b5d27c6e51a605b3b65a7e
Bryan <brn.dbn@gmail.com>
2026-02-16 17:14:24 -0600

    missed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,16 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './src/app/**/*.{ts,tsx,css,mdx}',
-    './src/components/**/*.{ts,tsx,mdx}',
-    './src/**/*.{ts,tsx,mdx}',
-    './package.json',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [typography, forms],
+    content: [
+        './src/app/**/*.{ts,tsx,css,mdx}',
+        './src/components/**/*.{ts,tsx,mdx}',
+        './src/**/*.{ts,tsx,mdx}',
+        './package.json',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [typography, forms],
 };
 
 export default config;
